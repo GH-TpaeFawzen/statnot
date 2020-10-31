624bde4a6185772453dea16d010f241f7901bb09
TpaeFawzen <30316430+GH-TpaeFawzen@users.noreply.github.com>
2020-10-31 21:49:17 +0900

    optimization
    In case you don't know, the POSIX lets you to surround matching pattern for case statement with parentheses, instead of just suffixing with right-parenthesis.

--- .statusline.sh
+++ .statusline.sh
@@ -1,7 +1,7 @@
-if [ $# -eq 0 ]; then
+case $# in (0)
     loadavg="`cat /proc/loadavg | awk '{print $1, $2, $3}'`";
     echo "[load ${loadavg}]   `date +'%R'`";
-else
+;;(*)
     echo "NOTIFICATION: $1";
-fi
+;;esac
 
